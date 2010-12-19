a0d9cf30955aeaaba4c7511504e2a60d3eb7ddcc
unknown <ethiele@.(none)>
2010-12-18 22:30:08 -0500

    updated

--- PLINQ_Benchmark/Program.cs
+++ PLINQ_Benchmark/Program.cs
@@ -9,18 +9,20 @@ namespace PLINQ_Benchmark
     {
         static void Main(string[] args)
         {
-            var vals = LoadList("vals.cvs");
+            var vals = LoadList("valsmore.cvs");
             List<long> lst = new List<long>();
             Console.WriteLine("Running on a system with {0} logical cores", System.Environment.ProcessorCount);
+            Console.WriteLine("Press enter to start");
+            Console.ReadLine();
             Console.WriteLine("Starting evaluation");
-            for (int i = 1; i < 4; i++)
+            for (int i = 1; i < 6; i++)
             {
                 var time = ProcessList(vals, i);
                 lst.Add(time);
                 Console.WriteLine("Using {0} core(s) took {1} ms", i.ToString(), time.ToString());
             }
             SaveList(lst, "results.cvs");
-            Console.ReadLine();
+            //Console.ReadLine();
             
         }
 
@@ -38,11 +40,16 @@ namespace PLINQ_Benchmark
             }
             sw.Stop();
             var counts = sc.end();
-            SaveList(counts[0], "CPU_total.csv");
+            //SaveList(counts[0], "CPU_total.csv");
+            List<string> headers = new List<string>();
+            headers.Add("CPU_total.csv");
             for (int i = 1; i < counts.Count; i++)
             {
-                SaveList(counts[0], string.Format("CPU_{0}_plvl{1}.csv", i - 1, plvl));
+                //SaveList(counts[0], string.Format("CPU_{0}_plvl{1}.csv", i - 1, plvl));
+                headers.Add(string.Format("CPU_{0}_plvl{1}", i - 1, plvl));
             }
+            SaveCSV(counts, headers.ToArray(), string.Format("CPU_Table_P{0}.csv", plvl));
+            
             return sw.ElapsedMilliseconds;   
         }
 
@@ -88,6 +95,39 @@ namespace PLINQ_Benchmark
                 sb.Remove(sb.Length - 1, 1);
             }
             System.IO.File.WriteAllText(str, sb.ToString());
+        }
+
+        static void SaveCSV(List<List<float>> tbl, string[] Headers, string filePath)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string str in Headers)
+            {
+                sb.Append(str);
+                sb.Append(",");
+            }
+            if (Headers.Length > 0)
+            {
+                sb.Remove(sb.Length - 1, 1);
+            }
+
+            sb.AppendLine();
+
+            int longestCol = (from p in tbl
+                              select p.Count).Max();
+
+            for (int y = 0; y < longestCol; y++)
+            {
+                for (int x = 0; x < tbl.Count; x++)
+                {
+                    if (tbl[x].Count > y)
+                    {
+                        sb.Append(tbl[x][y]);
+                    }
+                    sb.Append(',');
+                }
+                sb.AppendLine();
+            }
+            System.IO.File.WriteAllText(filePath, sb.ToString());
         }
 
         static List<long> LoadList(string str)
